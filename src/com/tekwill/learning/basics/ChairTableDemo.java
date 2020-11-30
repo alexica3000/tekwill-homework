7eaf79cfbc187272888ac6b989159a33237bef96
alex s <sanatateacom@gmail.com>
2020-11-30 19:44:54 +0200

    solve exercises from basics

---
+++ src/com/tekwill/learning/basics/ChairTableDemo.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class ChairTableDemo {
+    public static void main(String[] args) {
+        System.out.println("x                     x");
+        System.out.println("x                     x");
+        System.out.println("x                     x");
+        System.out.println("xxxx    xxxxxxx    xxxx");
+        System.out.println("x  x    x     x    x  x");
+        System.out.println("x  x    x     x    x  x");
+    }
+}
