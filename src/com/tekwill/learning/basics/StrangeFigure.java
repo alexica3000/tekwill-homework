793d5df1c8a3947fa8c471ccf4ce8e4829753c8c
alex s <sanatateacom@gmail.com>
2020-12-01 20:31:48 +0200

    add StrangeFigure class

---
+++ src/com/tekwill/learning/basics/StrangeFigure.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class StrangeFigure {
+    public static void main(String[] args) {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\ ");
+    }
+}
