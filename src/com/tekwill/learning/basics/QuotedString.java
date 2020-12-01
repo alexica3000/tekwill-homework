ef966d4028282d058108d3abe431e67e764b7aed
alex s <sanatateacom@gmail.com>
2020-12-01 20:25:17 +0200

    add QuotedString

---
+++ src/com/tekwill/learning/basics/QuotedString.java
@@ -0,0 +1,8 @@
+package com.tekwill.learning.basics;
+
+public class QuotedString {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is \n'much' better if you learn\nthe rules of \"escape sequences.\"\n");
+        System.out.println("Also, \"\" represents an empty String.\nDon't forget: use \\\" instead of \" !\n'' is not the same as \"");
+    }
+}
