5cc7ff5b47afd8bbbfb53d7e414a54b55541df7b
alex s <sanatateacom@gmail.com>
2020-12-03 20:56:04 +0200

    add alex and misp classes

---
+++ src/com/tekwill/learning/basics/Alex.java
@@ -0,0 +1,13 @@
+package com.tekwill.learning.basics;
+
+public class Alex {
+    public static void main(String[] args) {
+        System.out.println("      A          L              E E E E E    X     X ");
+        System.out.println("     A A         L              E             X   X  ");
+        System.out.println("    A   A        L              E              X X   ");
+        System.out.println("   A     A       L              E E E E E       X    ");
+        System.out.println("  A A A A A      L              E              X X   ");
+        System.out.println(" A         A     L         L    E             X   X  ");
+        System.out.println("A           A    L L L L L L    E E E E E    X     X ");
+    }
+}
---
+++ src/com/tekwill/learning/basics/Misp.java
@@ -0,0 +1,13 @@
+package com.tekwill.learning.basics;
+
+public class Misp {
+    public static void main(String[] args) {
+        System.out.println("M     M     IIIII      SSSSS      PPPPPP ");
+        System.out.println("MM   MM       I       S     S     P     P");
+        System.out.println("M M M M       I       S           P     P");
+        System.out.println("M  M  M       I        SSSSS      PPPPPP ");
+        System.out.println("M     M       I             S     P      ");
+        System.out.println("M     M       I       S     S     P      ");
+        System.out.println("M     M       I        SSSSS      P      ");
+    }
+}
