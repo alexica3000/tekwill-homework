5552ae57f076a0d0cb35681ebb1c9c1a30099c94
alex s <sanatateacom@gmail.com>
2020-12-08 20:13:29 +0200

    modified Knight and KnightDemo

--- src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/Knight.java
+++ src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/Knight.java
@@ -1,17 +1,41 @@
 package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;
 
 public class Knight {
-    int age;
-    String nickname;
-    static String guild;
+    private int age;
+    private String nickname;
+    public static String guild = "My kingdom";
 
-    // to be continue...
+    public Knight(int age, String nickname) {
+        this.age = age;
+        this.nickname = nickname;
+    }
 
-    void fight() {
+    public void fight() {
         System.out.println("Fight!!!");
     }
 
-    static void invokeMagic() {
-        System.out.println("Maaaagic");
+    public static void invokeMagic() {
+        System.out.println("Maaaagic!!!");
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "Knight: nickname is " + this.nickname + ", age - " + this.age + ".";
     }
 }
--- src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/KnightDemo.java
+++ src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/KnightDemo.java
@@ -1,5 +1,18 @@
 package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;
 
 public class KnightDemo {
+    public static void main(String[] args) {
+        Knight knight = new Knight(54, "Michael");
+        System.out.println("Name is " + knight.getNickname() + ", age - " + knight.getAge());
+        knight.fight();
 
+        Knight.invokeMagic();
+        System.out.println(Knight.guild);
+
+        knight.setAge(59);
+        knight.setNickname("Michael II");
+        System.out.println("Name is " + knight.getNickname() + ", age - " + knight.getAge());
+
+        System.out.println(knight.toString());
+    }
 }
