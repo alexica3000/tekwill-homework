8938509a0eda76199ec4df4f233e159b530a627a
alex s <sanatateacom@gmail.com>
2020-12-12 19:38:52 +0200

    add Constants class

---
+++ src/com/tekwill/learning/datatypes/Constants.java
@@ -0,0 +1,19 @@
+package com.tekwill.learning.datatypes;
+
+public class Constants {
+    public static int zeroNumber = 0;
+    public static int oneNumber = 1;
+    public static double piNumber = Math.PI;
+    public static double eNumber = Math.exp(1);
+    public static double squareRootTwo = Math.sqrt(2);
+    public static double goldenRatio = (1 + Math.sqrt(5)) / 2;
+
+    public static void main(String[] args) {
+        System.out.println("Zero: " + zeroNumber);
+        System.out.println("One: " + oneNumber);
+        System.out.println("Pi: " + piNumber);
+        System.out.println("e: " + eNumber);
+        System.out.println("Square: " + squareRootTwo);
+        System.out.println("Golden: " + goldenRatio);
+    }
+}
