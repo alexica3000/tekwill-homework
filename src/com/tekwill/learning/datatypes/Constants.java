54eba476e1c4c2d80876bf3dd773828ca2824d57
alex s <sanatateacom@gmail.com>
2020-12-14 18:44:35 +0200

    change constants

--- src/com/tekwill/learning/datatypes/Constants.java
+++ src/com/tekwill/learning/datatypes/Constants.java
@@ -1,19 +1,19 @@
 package com.tekwill.learning.datatypes;
 
 public class Constants {
-    public static int zeroNumber = 0;
-    public static int oneNumber = 1;
-    public static double piNumber = Math.PI;
-    public static double eNumber = Math.exp(1);
-    public static double squareRootTwo = Math.sqrt(2);
-    public static double goldenRatio = (1 + Math.sqrt(5)) / 2;
+    public static final int ZERO_NUMBER = 0;
+    public static final int ONE_NUMBER = 1;
+    public static final double PI_NUMBER = Math.PI;
+    public static final double E_NUMBER = Math.exp(1);
+    public static final double SQUARE_ROOT_TWO = Math.sqrt(2);
+    public static final double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
 
     public static void main(String[] args) {
-        System.out.println("Zero: " + zeroNumber);
-        System.out.println("One: " + oneNumber);
-        System.out.println("Pi: " + piNumber);
-        System.out.println("e: " + eNumber);
-        System.out.println("Square: " + squareRootTwo);
-        System.out.println("Golden: " + goldenRatio);
+        System.out.println("Zero: " + ZERO_NUMBER);
+        System.out.println("One: " + ONE_NUMBER);
+        System.out.println("Pi: " + PI_NUMBER);
+        System.out.println("e: " + E_NUMBER);
+        System.out.println("Square: " + SQUARE_ROOT_TWO);
+        System.out.println("Golden: " + GOLDEN_RATIO);
     }
 }
