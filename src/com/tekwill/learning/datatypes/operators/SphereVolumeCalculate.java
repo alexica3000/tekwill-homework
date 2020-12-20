d71139e9eb56cd5e91449baf09b2e1eea19d114d
alex s <sanatateacom@gmail.com>
2020-12-20 13:46:27 +0200

    calculate sphere volume

---
+++ src/com/tekwill/learning/datatypes/operators/SphereVolumeCalculate.java
@@ -0,0 +1,11 @@
+package com.tekwill.learning.datatypes.operators;
+
+public class SphereVolumeCalculate {
+    public static void main(String[] args) {
+        int radius = 10;
+
+        double volume = (4.0f/3.0f) * Math.PI * 10 * 10 * 10;
+
+        System.out.println("Volume is: " + volume);
+    }
+}
