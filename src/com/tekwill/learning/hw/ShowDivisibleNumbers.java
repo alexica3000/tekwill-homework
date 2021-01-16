a02ed3e2ed7d30d7ed9d5f87e3fc75c5fcaeb944
alex s <sanatateacom@gmail.com>
2021-01-16 15:26:38 +0200

    display divisible numbers

---
+++ src/com/tekwill/learning/hw/ShowDivisibleNumbers.java
@@ -0,0 +1,21 @@
+package com.tekwill.learning.hw;
+
+public class ShowDivisibleNumbers {
+    private int nrOutputs = 0;
+
+    public void printDivisibleNumbers(int from, int to, int perLine) {
+        for (int i = from; i <= to; i++) {
+            if (i % 5 == 0 && i % 6 == 0) {
+                System.out.print(i + " ");
+                this.nrOutputs++;
+                this.printLine(perLine);
+            }
+        }
+    }
+
+    private void printLine(int perLine) {
+        if (this.nrOutputs % perLine == 0) {
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/tekwill/learning/hw/ShowDivisibleNumbersDemo.java
@@ -0,0 +1,8 @@
+package com.tekwill.learning.hw;
+
+public class ShowDivisibleNumbersDemo {
+    public static void main(String[] args) {
+        ShowDivisibleNumbers service = new ShowDivisibleNumbers();
+        service.printDivisibleNumbers(100, 1000, 10);
+    }
+}
