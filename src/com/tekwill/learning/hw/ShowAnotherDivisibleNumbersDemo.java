c5e807df4d9d11890e221fc149175213ddf4e4d1
alex s <sanatateacom@gmail.com>
2021-01-16 15:35:27 +0200

    diplay all the numbers divisible by 5 or 6, but not both

---
+++ src/com/tekwill/learning/hw/ShowAnotherDivisibleNumbersDemo.java
@@ -0,0 +1,8 @@
+package com.tekwill.learning.hw;
+
+public class ShowAnotherDivisibleNumbersDemo {
+    public static void main(String[] args) {
+        ShowAnotherDivisibleNumbers service = new ShowAnotherDivisibleNumbers();
+        service.printDivisibleNumbers(100, 200, 10);
+    }
+}
