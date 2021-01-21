65ab2f1ffc2b23ad57177c99ea3c6cfa68f58c62
alex s <sanatateacom@gmail.com>
2021-01-21 20:30:58 +0200

    processing a string and display odd characters

--- src/com/tekwill/learning/hw/StringProcessor.java
+++ src/com/tekwill/learning/hw/StringProcessor.java
@@ -2,18 +2,20 @@ package com.tekwill.learning.hw;
 
 import java.util.Scanner;
 
-public class StringReverser {
+public class StringProcessor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String string = scanner.nextLine();
-        String reversedString = "";
+        String newString = "";
 
-        for (int i = string.length() - 1; i >= 0; i--) {
-            reversedString += string.charAt(i);
+        for (int i = 0; i <= string.length() - 1; i++) {
+            if (i % 2 == 0) {
+                newString += string.charAt(i);
+            }
         }
 
-        System.out.println(reversedString);
+        System.out.println(newString);
     }
 }
