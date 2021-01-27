c0a275158e6298692ff6e1e790118fd653b607e6
alex s <sanatateacom@gmail.com>
2021-01-27 23:15:47 +0200

    convert roman to numerals

---
+++ src/com/tekwill/learning/hw/RomanToNumeralsConverter.java
@@ -0,0 +1,62 @@
+package com.tekwill.learning.hw;
+
+import java.util.Scanner;
+
+public class RomanToNumeralsConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a roman numeral:");
+        String input = scanner.next();
+        System.out.println(convertRomanNumerals(input));
+    }
+
+    static int convertRomanNumerals(String roman) {
+        int result = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            if (!isValidRoman(roman.charAt(i))) {
+                System.out.println("Wrong input!!!!");
+                break;
+            }
+
+            if (i + 1 < roman.length() && adjacentRomanNeedSubtracting(roman.charAt(i), roman.charAt(i + 1))) {
+                result += (romanToInt(roman.charAt(i + 1)) - romanToInt(roman.charAt(i)));
+                i++;
+            } else {
+                result += romanToInt(roman.charAt(i));
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isValidRoman(char c) {
+        return c == 'I' || c == 'V' || c == 'X' || c == 'L' || c == 'C' || c == 'D' || c == 'M';
+    }
+
+    private static boolean adjacentRomanNeedSubtracting(char a, char b) {
+        return ((a == 'I' && (b == 'V' || b == 'X')) || (a == 'X' && (b == 'L' || b == 'C')) || (a == 'C' && (b == 'D' || b == 'M')));
+    }
+
+    public static int romanToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
+}
