8ded56e6d7e36c67c4edc2de097520b12faffeeb
alex s <sanatateacom@gmail.com>
2021-02-10 22:53:08 +0200

    examples with inheritance, interfaces

---
+++ src/inheritance/interfaces/devicezx/SmartHomeDemo.java
@@ -0,0 +1,14 @@
+package inheritance.interfaces.devicezx;
+
+public class SmartHomeDemo {
+    public static void main(String[] args) {
+        SmartHomeController smartHomeController = new SmartHomeController(new HallRoomLight(), new MusicSpeaker(), new TV());
+
+        smartHomeController.turnOnAllDevices();
+        smartHomeController.turnOffAllDevices();
+        smartHomeController.turnOnAllDevices();
+        smartHomeController.emergencyPowerOff();
+
+        smartHomeController.updateDevices();
+    }
+}
