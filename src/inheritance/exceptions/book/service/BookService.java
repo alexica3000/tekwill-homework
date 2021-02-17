06fd00adc20a6ed1ffa67419db7f647e2d8a06bb
alex s <sanatateacom@gmail.com>
2021-02-17 23:07:43 +0200

    exceptions

---
+++ src/inheritance/exceptions/book/service/BookService.java
@@ -0,0 +1,9 @@
+package inheritance.exceptions.book.service;
+
+import inheritance.exceptions.book.domain.Book;
+import inheritance.exceptions.book.domain.exceptions.BookNotFoundCheckedException;
+import inheritance.exceptions.book.domain.exceptions.BookNotFoundRuntimeException;
+
+public interface BookService {
+    Book getBookByIsbn(String isbn) throws BookNotFoundRuntimeException, NullPointerException, BookNotFoundCheckedException;
+}
